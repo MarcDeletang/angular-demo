b496654c2211fd67499b0a4a8ab7e41792a2e9a1
marc d <marc.deletang@gmail.com>
2016-12-14 23:21:50 +0100

    removed

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-
-var miniAPI = require('./mini-api.js').init(app)
-require('./gulpfile.js').watch()
-
-app.use(express.static('www'))
-app.listen(1337)
--- tasks/task1.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp')
-
-
-var firstTask = function(){
-	console.log("I don't do much.")
-}
-
-gulp.task('task1', firstTask)
