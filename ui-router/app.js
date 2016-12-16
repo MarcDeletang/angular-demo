5e8d38ce329ff5df107d6b4c3f025d1de1d550b5
marc d <marc.deletang@gmail.com>
2016-12-16 11:43:05 +0100

    added url params

--- ui-router/app.js
+++ ui-router/app.js
@@ -13,7 +13,8 @@ var miniAPI = require('./mini-api.js').init(app)
 
 app.use(express.static('www'))
 app.use('/foo', express.static('www/index.html'))
-//app.use('/error', express.static('www/index.html'))
+app.use('/foo/:id', express.static('www/index.html'))
+app.use('/error', express.static('www/index.html'))
 
 var port = 1337
 console.log('listen', port)
--- ui-router/assets/js/app.js
+++ ui-router/assets/js/app.js
@@ -1,6 +1,6 @@
 var Todo = angular.module('Todo', ['ui.router', 'ui.bootstrap'])
 	.config(function ($stateProvider, $locationProvider, $urlRouterProvider, $qProvider) {
-		$urlRouterProvider.otherwise('/error')
+		$urlRouterProvider.otherwise('/errorLocation')
 		$locationProvider.html5Mode(true)
 		$qProvider.errorOnUnhandledRejections(false)
 
@@ -21,13 +21,17 @@ var Todo = angular.module('Todo', ['ui.router', 'ui.bootstrap'])
 				controller: 'FooController',
 				templateUrl: 'templates/foo.html',
 			})
+			.state('fooWithParam', {
+				url: '/foo/:param',
+				controller: 'FooController',
+				templateUrl: 'templates/foo.html',
+			})
 			.state('impossible', {
 				url: '/admin',
 				controller: 'FooController',
 				templateUrl: 'templates/foo.html',
 				resolve: {
 					isConnected: function ($q) {
-						//return $q.resolve('ok')
 						return $q.reject('error')
 					},
 				}
@@ -52,6 +56,6 @@ var Todo = angular.module('Todo', ['ui.router', 'ui.bootstrap'])
 	.controller('MainController', function ($scope) {
 		$scope.message = 'This dashboard is empty'
 	})
-	.controller('FooController', function ($scope) {
-		$scope.message = 'Dang this is worse:s'
+	.controller('FooController', function ($scope,  $stateParams) {
+		$scope.message = 'Dang this is worse:s' + $stateParams.param
 	})
